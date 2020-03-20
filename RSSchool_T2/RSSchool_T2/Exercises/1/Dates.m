7c7878171f1cff48e050300469eb0045e6e1bfaa
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-20 12:32:02 +0300

    feat: implement task1 - DateFormatter

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,21 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSString* newDay = [NSString stringWithFormat:@"%d%d", [day intValue]/10, [day intValue]%10];
+    NSString* newMonth = [NSString stringWithFormat:@"%d%d", [month intValue]/10, [month intValue]%10];
+
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateStyle = NSDateFormatterLongStyle;
+    dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"ru_BY"];
+    [dateFormatter setDateFormat:@"dd-MM-yyyy"];
+    
+    NSDate *date = [dateFormatter dateFromString: [NSString stringWithFormat:@"%@-%@-%@", newDay, newMonth, year]];
+    [dateFormatter setDateFormat:@"dd MMMM, EEEE"];
+    if (date) {
+        return [dateFormatter stringFromDate:date];
+    } else {
+        return @"Такого дня не существует";
+    }
 }
 
 @end
