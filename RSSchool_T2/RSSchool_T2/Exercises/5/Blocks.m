b0c75d795e80879692bed7d49209ca4b0700f779
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-23 00:57:57 +0300

    feat: implement task5 - Blocks

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,6 +1,36 @@
 #import "Blocks.h"
 
+@interface Blocks()
+
+@property (nonatomic) NSArray* array;
+
+@end
+
 @implementation Blocks
+
+-(BlockA)blockA {
+    return [^(NSArray* array){
+        self.array = array;
+    } copy];
+}
+
+-(BlockB)blockB {
+    return [^(Class class) {
+        NSArray* filteredArray = [self.array filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id  _Nullable evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+            return [evaluatedObject isKindOfClass:class];
+        }]];
+        if (class == [NSNumber class]) {
+            self.blockC([filteredArray valueForKeyPath:@"@sum.self"]);
+        } else if (class == [NSString class]) {
+            self.blockC([filteredArray componentsJoinedByString:@""]);
+        } else if (class == [NSDate class]) {
+            filteredArray = [filteredArray sortedArrayUsingSelector:@selector(compare:)];
+            NSDateFormatter* formatter = [NSDateFormatter new];
+            [formatter setDateFormat:@"dd.MM.yyyy"];
+            self.blockC([formatter stringFromDate:[filteredArray lastObject]]);
+        }
+    } copy];
+}
 
 
 @end
