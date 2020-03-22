4b763cc145453a3d97ff4c0d63dabd580d1ef642
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-22 21:43:47 +0300

    feat: implement task3 - Abbreviation

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,26 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    
+    NSMutableString* mutableA = [NSMutableString stringWithString:a];
+    NSUInteger i = 0;
+    NSUInteger j = 0;
+    
+    while (j < [b length] && [mutableA length] >= [b length]) {
+        
+        if (![[[mutableA substringWithRange:NSMakeRange(i, 1)] capitalizedString] isEqualToString:[[b substringWithRange:NSMakeRange(j, 1)] capitalizedString]]) {
+            [mutableA replaceCharactersInRange:NSMakeRange(i, 1) withString:@""];
+        } else {
+            [mutableA replaceCharactersInRange:NSMakeRange(i, 1) withString:[b substringWithRange:NSMakeRange(j, 1)]];
+            i++;
+            j++;
+        }
+    }
+    
+    if ([mutableA containsString:b]){
+        return @"YES";
+    } else {
+        return @"NO";
+    }
 }
 @end
