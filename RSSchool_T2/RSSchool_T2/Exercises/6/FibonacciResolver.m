42e279b0ea2db2368a9b3d2159a811fc63cf3e2d
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-23 12:50:29 +0300

    feat: implement task6 - Fibonacci numbers

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,19 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    NSMutableArray<NSNumber*>* fibNumbers = [NSMutableArray arrayWithArray:@[@0,@1]];
+    while ([fibNumbers[fibNumbers.count - 1] intValue] * [fibNumbers[fibNumbers.count - 2] intValue] < [number intValue]) {
+        NSNumber* sum = [NSNumber numberWithInt:[fibNumbers[fibNumbers.count - 1] intValue] + [fibNumbers[fibNumbers.count - 2] intValue]];
+        [fibNumbers addObject:sum];
+    }
+    NSLog(@"%@", fibNumbers);
+    NSNumber* result;
+    if ([fibNumbers[fibNumbers.count - 1] intValue] * [fibNumbers[fibNumbers.count - 2] intValue] == [number intValue]) {
+        result = @1;
+    } else {
+        result = @0;
+    }
+    
+    return @[fibNumbers[fibNumbers.count - 2], fibNumbers[fibNumbers.count - 1], result];
 }
 @end
