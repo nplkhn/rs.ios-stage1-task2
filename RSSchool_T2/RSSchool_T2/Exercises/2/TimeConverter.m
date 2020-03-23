b81a3c40fd8cb09a5ec004392706e78b76da2cc1
Nikita Plakhin <nplkhn1@yandex.ru>
2020-03-23 23:03:32 +0300

    fix: correct implementation of task2 - TimeInWords

--- RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
+++ RSSchool_T2/RSSchool_T2/Exercises/2/TimeConverter.m
@@ -3,33 +3,26 @@
 @implementation TimeConverter
 // Complete the following function
 - (NSString*)convertFromHours:(NSString*)hours minutes:(NSString*)minutes {
-    NSNumberFormatter* numberFormatter = [NSNumberFormatter new];
-    [numberFormatter setLocale:[NSLocale localeWithLocaleIdentifier:@"en_GB"]];
-    [numberFormatter setNumberStyle:NSNumberFormatterSpellOutStyle];
-    
+    NSDictionary *dict = [NSDictionary dictionaryWithObjects:@[@"one", @"two", @"three", @"four", @"five", @"six", @"seven", @"eight", @"nine", @"ten", @"eleven", @"twelve", @"thirteen", @"fourteen", @"fifteen", @"sixteen", @"seventeen", @"eighteen", @"nineteen", @"twenty", @"twenty one", @"twenty two", @"twenty three", @"twenty four", @"twenty five", @"twenty six", @"twenty seven", @"twenty eight", @"twenty nine"] forKeys:@[@1,@2,@3,@4,@5,@6,@7,@8,@9,@10,@11,@12,@13,@14, @15, @16, @17, @18, @19, @20, @21, @22, @23, @24, @25, @26, @27, @28, @29]];
     int intHours = [hours intValue];
     int intMinutes = [minutes intValue];
-    if (intMinutes >= 60 || intHours >= 24 || intMinutes < 0 || intHours < 0) {
+    
+    if (intMinutes > 59) {
         return @"";
-    }
-    if (intMinutes == 0) {
-        return [NSString stringWithFormat:@"%@ o' clock", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intHours]]];
+    } else if (intMinutes == 0) {
+        return [NSString stringWithFormat:@"%@ o' clock", [dict objectForKey:[NSNumber numberWithInt:intHours]]];
     } else if (intMinutes == 1) {
-    return [NSString stringWithFormat: @"%@ minute past %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intMinutes]], [numberFormatter stringFromNumber: [NSNumber numberWithInt:intHours]]];
+        return [NSString stringWithFormat:@"%@ minute past %@", [dict objectForKey:[NSNumber numberWithInt:intMinutes]], [dict objectForKey:[NSNumber numberWithInt:intHours]]];
     } else if (intMinutes == 15) {
-        return [NSString stringWithFormat:@"quarter past %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intHours]]];
+        return [NSString stringWithFormat:@"quarter past %@", [dict objectForKey:[NSNumber numberWithInt:intHours]]];
     } else if (intMinutes == 45) {
-        return [NSString stringWithFormat:@"quarter to %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intHours + 1]]];
+        return [NSString stringWithFormat:@"quarter to %@", [dict objectForKey:[NSNumber numberWithInt:intHours + 1]]];
     } else if (intMinutes == 30) {
-        return [NSString stringWithFormat:@"half past %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intHours]]];
+        return [NSString stringWithFormat:@"half past %@", [dict objectForKey:[NSNumber numberWithInt:intHours]]];
     } else if (intMinutes < 30) {
-        NSMutableString* timeString = [NSMutableString stringWithFormat: @"%@ minutes past %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:intMinutes]], [numberFormatter stringFromNumber: [NSNumber numberWithInt:intHours]]];
-        [timeString replaceOccurrencesOfString:@"-" withString:@" " options:NSCaseInsensitiveSearch range:NSMakeRange(0, [timeString length])];
-        return timeString;
+        return [NSString stringWithFormat:@"%@ minutes past %@", [dict objectForKey:[NSNumber numberWithInt:intMinutes]], [dict objectForKey:[NSNumber numberWithInt:intHours]]];
     } else {
-        NSMutableString* timeString = [NSMutableString stringWithFormat: @"%@ minutes to %@", [numberFormatter stringFromNumber:[NSNumber numberWithInt:60 - intMinutes]], [numberFormatter stringFromNumber: [NSNumber numberWithInt:1 + intHours]]];
-        [timeString replaceOccurrencesOfString:@"-" withString:@" " options:NSCaseInsensitiveSearch range:NSMakeRange(0, [timeString length])];
-        return timeString;;
+        return [NSString stringWithFormat:@"%@ minutes to %@", [dict objectForKey:[NSNumber numberWithInt:60 - intMinutes]], [dict objectForKey:[NSNumber numberWithInt:intHours + 1]]];
     }
 }
 @end
